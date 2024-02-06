802f5133f4020cfa80be0a039f8ae95c4d43e4aa
octavi42 <53470992+octavi42@users.noreply.github.com>
2024-02-06 15:41:23 +0200

    audio id for multiple request

--- blueprints/functions/speech_tools.py
+++ blueprints/functions/speech_tools.py
@@ -3,6 +3,7 @@ import os
 from utils.audio_interpretation import get_interpretation_from_prompt
 from speech_to_text.choose_model import process_audio, process_audio_with_language
 from utils.translator import translate
+import uuid
 
 speech_tools_pb = Blueprint('speech_tools', __name__)
 
@@ -14,11 +15,11 @@ def audio_interpret():
 
         if audio_file:
             # Save the audio file temporarily
-            audio_path = "stored_audio/temp_audio.wav"
-            audio_file.save(audio_path)
+            unique_filename = f"stored_audio/{uuid.uuid4()}.wav"
+            audio_file.save(unique_filename)
 
             # Process the audio and get the result
-            result = process_audio(audio_path)
+            result = process_audio(unique_filename)
 
             # Interpret the result
             interpreted_result = get_interpretation_from_prompt(result)
@@ -27,7 +28,7 @@ def audio_interpret():
             interpreted_result.update({"prompt": result})
 
             # Delete the audio file
-            os.remove(audio_path)
+            os.remove(unique_filename)
 
             # Return the interpreted result
             return jsonify({"interpreted_result": interpreted_result})
@@ -45,14 +46,14 @@ def audio_interpret_with_language():
 
         if audio_file and language:
             # Save the audio file temporarily
-            audio_path = "stored_audio/temp_audio.wav"
-            audio_file.save(audio_path)
+            unique_filename = f"stored_audio/{uuid.uuid4()}.wav"
+            audio_file.save(unique_filename)
 
             if language != "english" and language != "arabic":
                 return jsonify({"error": "Language not supported, (arabic / english)"}), 400
 
             # Process the audio and get the result
-            result = process_audio_with_language(audio_path, language)
+            result = process_audio_with_language(unique_filename, language)
 
             # Interpret the result
             interpreted_result = get_interpretation_from_prompt(result)
@@ -66,7 +67,7 @@ def audio_interpret_with_language():
             interpreted_result.update({"prompt": result})
 
             # Delete the audio file
-            os.remove(audio_path)
+            os.remove(unique_filename)
 
             # Return the interpreted result
             return jsonify({"interpreted_result": interpreted_result})
@@ -84,14 +85,14 @@ def speech_to_text():
 
         if audio_file:
             # Save the audio file temporarily
-            audio_path = "stored_audio/temp_audio.wav"
-            audio_file.save(audio_path)
+            unique_filename = f"stored_audio/{uuid.uuid4()}.wav"
+            audio_file.save(unique_filename)
 
             # Process the audio and get the result
-            result = process_audio(audio_path)
+            result = process_audio(unique_filename)
 
             # Delete the audio file
-            os.remove(audio_path)
+            os.remove(unique_filename)
 
             # Return the interpreted result
             return jsonify({"interpreted_result": result})
@@ -109,14 +110,14 @@ def speech_to_text_with_language():
 
         if audio_file and language:
             # Save the audio file temporarily
-            audio_path = "stored_audio/temp_audio.wav"
-            audio_file.save(audio_path)
+            unique_filename = f"stored_audio/{uuid.uuid4()}.wav"
+            audio_file.save(unique_filename)
 
             # Process the audio and get the result
-            result = process_audio_with_language(audio_path, language)
+            result = process_audio_with_language(audio_file, language)
 
             # Delete the audio file
-            os.remove(audio_path)
+            os.remove(audio_file)
 
             # Return the interpreted result
             return jsonify({"interpreted_result": result})
